35c6dbd2e29ecae18842b662904030f1cdb145bf
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-10-15 10:34:04 +0200

    Control Flow - @For

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -12,6 +12,9 @@ export default class ControlFlowComponent {
   public showContent = signal(false);
   public grade = signal<Grade>('A');
 
+  public frameworks = signal(['Angular', 'Vue', 'Svelt', 'Qwik', 'React']);
+  public frameworks2 = signal([]);
+
   public toggleContent() {
     this.showContent.update((value) => !value);
   }
