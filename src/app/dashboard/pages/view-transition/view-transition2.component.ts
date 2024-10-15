ad5d69c1b1b880cf54d65fb5c0845c8053bb4c02
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-10-15 11:51:45 +0200

    Hero Animation - View Transitions

--- src/app/dashboard/pages/view-transition/view-transition2.component.ts
+++ src/app/dashboard/pages/view-transition/view-transition2.component.ts
@@ -1,5 +1,5 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
@@ -8,7 +8,7 @@ import { TitleComponent } from '@shared/title/title.component';
   template: `
     <app-title title="View Transition 2" />
 
-    <section class="flex justify-start">
+    <section class="flex justify-end">
       <img
         srcset="https://picsum.photos/id/237/200/300"
         alt="Picsum"
@@ -18,7 +18,7 @@ import { TitleComponent } from '@shared/title/title.component';
       />
 
       <div
-        class="bg-blue-500 w-56 h-56"
+        class="fixed bottom-16 right-10 bg-blue-800 w-32 h-32 rounded"
         style="view-transition-name: hero2"
       ></div>
     </section>
